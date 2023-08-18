7c4ef72d13baed06665d59f909bea8350cb1c46a
Shadi Masoumi <shadimasoumiphd@gmail.com>
2023-08-18 15:21:48 -0400

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,9 @@ release = '2023'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+matlab_src_dir = os.path.dirname(os.path.abspath(__file__))
+matlab_show_property_default_value = True
+matlab_short_links = True
 extensions = ['sphinxcontrib.matlab', 'sphinx.ext.autodoc']
 
 primary_domain = "mat"
